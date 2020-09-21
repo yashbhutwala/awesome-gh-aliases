ed0f72a23e1a30a40cf65d16897b9646ad22539b
Yash Bhutwala <ymb002@bucknell.edu>
2020-09-21 10:46:18 -0400

    add script for list files changed in a pr

--- scripts/generate-aliases.sh
+++ scripts/generate-aliases.sh
@@ -16,30 +16,50 @@ gh alias set --shell igrep 'gh issue list --label="$1" | grep -i $2'
 ################################################################################
 # custom API
 #
-# user
-gh alias set "user" 'api user'
+# user: fetch information about the currently authenticated user as JSON
+gh alias set 'user' 'api user'
 #
-# list-milestone
+# list-milestones: list the open milestones for the current repository.
 # '"'"' means a single single quote, see: https://stackoverflow.com/a/1250279
-gh alias set --shell "list-milestones" 'gh api --paginate graphql -F owner='"'"':owner'"'"' -F name='"'"':repo'"'"' -f query='"'"'
-    query($per_page: Int = 100, $endCursor: String, $name: String!, $owner: String!) {
-      repository(owner: $owner, name: $name) {
-        milestones(first: $per_page, after: $endCursor, states: OPEN, orderBy: {field:CREATED_AT, direction:DESC}) {
-          nodes {
-            title
-            number
-          }
-          pageInfo {
-            hasNextPage
-            endCursor
+gh alias set --shell 'list-milestones' \
+  'gh api --paginate graphql -F owner='"'"':owner'"'"' -F name='"'"':repo'"'"' -f query='"'"'
+      query($per_page: Int = 100, $endCursor: String, $owner: String!, $name: String!) {
+        repository(owner: $owner, name: $name) {
+          milestones(first: $per_page, after: $endCursor, states: OPEN, orderBy: {field:CREATED_AT, direction:DESC}) {
+            nodes {
+              title
+              number
+            }
+            pageInfo {
+              hasNextPage
+              endCursor
+            }
           }
         }
       }
-    }
-  '"'"' | jq -r ".data.repository.milestones.nodes[] | [.number,.title] | @tsv"'
+    '"'"' | jq -r ".data.repository.milestones.nodes[] | [.number,.title] | @tsv"'
 #
-#
-
+# pr-files-changed: lists files changed in a specific pull request.
+gh alias set --shell 'pr-files-changed' \
+  'gh api --paginate graphql -F owner='"'"':owner'"'"' -F name='"'"':repo'"'"' -F "pr=$1" -f query='"'"'
+      query($per_page: Int = 100, $endCursor: String, $pr: Int!, $owner: String!, $name: String!) {
+        repository(owner: $owner, name: $name) {
+          pullRequest(number: $pr) {
+            files(first: $per_page, after: $endCursor) {
+              edges {
+                node {
+                  path
+                }
+              }
+              pageInfo {
+                endCursor
+                hasNextPage
+              }
+            }
+          }
+        }
+      }
+    '"'"' | jq -r ".data.repository.pullRequest.files.edges[] | [.node.path] | @tsv"'
 ################################################################################
 
 
