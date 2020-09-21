b007ff0e790d95e1fc9b7d92b3b02a36b08341a8
Yash Bhutwala <ymb002@bucknell.edu>
2020-09-21 11:56:10 -0400

    add search-repos script

--- scripts/generate-aliases.sh
+++ scripts/generate-aliases.sh
@@ -16,10 +16,10 @@ gh alias set --shell igrep 'gh issue list --label="$1" | grep -i $2'
 ################################################################################
 # custom API
 #
-# user: fetch information about the currently authenticated user as JSON
+# user : fetch information about the currently authenticated user as JSON
 gh alias set 'user' 'api user'
 #
-# list-milestones: list the open milestones for the current repository.
+# list-milestones : list the open milestones for the current repository.
 # '"'"' means a single single quote, see: https://stackoverflow.com/a/1250279
 gh alias set --shell 'list-milestones' \
   'gh api --paginate graphql -F owner='"'"':owner'"'"' -F name='"'"':repo'"'"' -f query='"'"'
@@ -39,7 +39,7 @@ gh alias set --shell 'list-milestones' \
       }
     '"'"' | jq -r ".data.repository.milestones.nodes[] | [.number,.title] | @tsv"'
 #
-# pr-files-changed: lists files changed in a specific pull request.
+# pr-files-changed <PR_NUMBER> : lists files changed in a specific pull request.
 gh alias set --shell 'pr-files-changed' \
   'gh api --paginate graphql -F owner='"'"':owner'"'"' -F name='"'"':repo'"'"' -F "pr=$1" -f query='"'"'
       query($per_page: Int = 100, $endCursor: String, $pr: Int!, $owner: String!, $name: String!) {
@@ -61,7 +61,7 @@ gh alias set --shell 'pr-files-changed' \
       }
     '"'"' | jq -r ".data.repository.pullRequest.files.edges[] | [.node.path] | @tsv"'
 #
-# list-repos: list repositories of the supplied user
+# list-repos <USER> : list repositories of the supplied user
 gh alias set --shell 'list-repos' \
   'gh api --paginate graphql -F owner="$1" -f query='"'"'
       query($owner: String!, $per_page: Int = 100, $endCursor: String) {
@@ -79,3 +79,24 @@ gh alias set --shell 'list-repos' \
       }
     '"'"' | jq -r ".data.repositoryOwner.repositories.nodes[] | [.nameWithOwner] | @tsv"'
 ################################################################################
+#
+# search-repos <N> <QUERY> : List N repositories and their stars count that match <QUERY>.
+gh alias set --shell 'search-repos' \
+  'gh api graphql -F per_page="$1" -F q="$2" -f query='"'"'
+      query($q: String!, $per_page: Int = 10, $endCursor: String) {
+        search(query: $q, type: REPOSITORY, first: $per_page, after: $endCursor) {
+          nodes {
+            ...on Repository {
+              nameWithOwner
+              stargazers {
+                totalCount
+              }
+            }
+          }
+          pageInfo {
+            hasNextPage
+            endCursor
+          }
+        }
+      }
+    '"'"' | jq -r ".data.search.nodes[] | [.nameWithOwner,.stargazers.totalCount] | @tsv"'
